6c0aab02dcb6c07d22315f93c1042d2a5dd445e6
Mohit Vadsak <mohitvadsak107@gmail.com>
2020-07-16 12:12:34 +0530

    Week 5 Added

---
+++ Week 5/index.php
@@ -0,0 +1,18 @@
+<title>Mohit Vadsak PHP</title>
+<h1>Mohit Vadsak PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Mohit Vadsak\" is ".hash('sha256', 'Mohit Vadsak');
+?></p>
+<pre>ASCII ART:
+	****       **** 
+	*****     *****
+	**  **   **  **
+	**   ** **   ** 
+	**    ***    **
+	**    ***    **
+	**           **
+	**           **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+<br>
+<a href="check.php">Click here to cause a traceback</a>
